c9e06d61f2a5cf6fcf6d8abef19924dabd38a126
Vinodkumar9429 <umr09347@gmail.com>
2025-05-31 08:43:44 +0530

    Dynamic completed log added

--- src/main.ts
+++ src/main.ts
@@ -24,22 +24,32 @@ inputFields.forEach(()=>{
     check.parentElement?.classList.toggle('completed');
     const tick = check.querySelector('.check');
     tick?.classList.toggle('hidden');
-    if(i == 0){
-      progress.classList.toggle('w-[34%]')
-      progressText.textContent = `${i+1}/3 Completed`;
-    }
-    else if(i == 1){
-      progress.classList.toggle('w-[68%]')
-      progressText.textContent = `${i+1}/3 Completed`;
-      keepGoing.textContent = "Almost there, just hang on!";
-      
-    }
-    else{
-      progress.classList.toggle('w-[100%]')
-      progressText.textContent = `${i+1}/3 Completed`;
-      keepGoing.textContent = "You've done it!";
-      
-    }
+
+const completedCount = Array.from(checkboxes).filter(c => c.parentElement?.classList.contains('completed')).length;
+
+progress.classList.remove('w-[34%]', 'w-[68%]', 'w-full');
+
+
+if (completedCount === 1) {
+  progress.classList.add('w-[34%]');
+  progressText.textContent = `1/3 Completed`;
+  keepGoing.textContent = "Keep going";
+} 
+else if (completedCount === 2) {
+  progress.classList.add('w-[68%]');
+  progressText.textContent = `2/3 Completed`;
+  keepGoing.textContent = "Almost there, just hang on!";
+}
+else if (completedCount === 3) {
+  progress.classList.add('w-full');
+  progressText.textContent = `3/3 Completed`;
+  keepGoing.textContent = "You've done it!";
+}
+else {
+  progressText.textContent = "";
+  keepGoing.textContent = "Add tasks, get going";
+}
+
     
   }
   
@@ -60,7 +70,7 @@ clear.addEventListener('click', ()=>{
   })
 
   progress.classList.remove('w-[34%]', 'w-[68%]', 'w-[100%]');
-  progress.classList.add('w-[0%]');
+  progress.classList.remove('w-full');
   progressText.textContent = "";
 
   keepGoing.textContent = 'Cleared All the Inputs';
